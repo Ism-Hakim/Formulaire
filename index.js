e4cbceabdf5cf5990060421895c0bd9e320d9689
Hakim Le Boss <sasaki1300397@gmail:com>
2023-11-02 19:01:48 +0100

    mise en place du serveur express

---
+++ index.js
@@ -0,0 +1,17 @@
+// importation express
+import { Express } from "express";
+// importaion dotenv
+import 'dotenv/config'
+
+import router from "./app/router.js";
+
+import express from "express";
+import 'dotenv/config';
+
+const port = process.env.PORT || 3400
+
+const app = express()
+
+app.listen(port, () => {
+    console.log(`serveur en écoute sur http://localhost:${port}`)
+})
