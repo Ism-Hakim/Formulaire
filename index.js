7131b10f44f9e7daad3051ba186e5c312cc8797e
Hakim Le Boss <sasaki1300397@gmail:com>
2023-11-02 19:05:39 +0100

    chemin vers les fichiers ejs

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const port = process.env.PORT || 3400
 
 const app = express()
 
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
 app.listen(port, () => {
     console.log(`serveur en écoute sur http://localhost:${port}`)
 })
