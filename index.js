64fa2fc9a7a9bf2aff935483649451b6c348d8fb
Hakim Le Boss <sasaki1300397@gmail:com>
2023-11-03 20:37:14 +0100

    body css

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-// importation express
-import { Express } from "express";
-// importaion dotenv
-import 'dotenv/config'
-
-import router from "./app/router.js";
-
 import express from "express";
 import 'dotenv/config';
 
-const port = process.env.PORT || 3400
-
-const app = express()
+const port = process.env.PORT || 3400;
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+app.use(express.static('./public')); 
+
 app.listen(port, () => {
-    console.log(`serveur en écoute sur http://localhost:${port}`)
-})
+    console.log(`Le serveur est en écoute sur http://localhost:${port}`);
+});
